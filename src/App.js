c8e03a7690b7b78eedd605063b701dd6996c406f
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-10-02 09:41:41 +0200

    add accueil

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Content from './component/Content';
+import Content from './component/wrapper/Content';
+import Accueil from './component/accueil/Accueil';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Content title="toto" />}/>
+        <Route path="/" element={<Accueil />}/>
+        {/* <Route path="/" element={<Content title="toto" />}/> */}
       </Routes>
     </BrowserRouter>
   );
---
+++ src/component/accueil/Accueil.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Form from '../wrapper/Form';
+import Header from '../wrapper/Header';
+import Footer from '../wrapper/Footer';
+
+function Accueil() {
+  return (
+    <>
+    <Header/>
+    <h2>Accueil</h2>
+    <div>Content</div>
+    <br/>
+    <Form />
+    <Footer/>
+    </>
+  )
+}
+
+export default Accueil;
