191f4b58dda5a08b5980236d12f3940512cebe96
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-10-03 10:28:30 +0200

    bug fix

--- src/component/MyCars.js
+++ src/component/MyCars.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Wrapper from './Wrapper';
 import Car from './Car';
 
-const [cars, setCar] = useState([
-    { marque: 'Renault', couleur: 'Rouge' },
-    { marque: 'Honda', couleur: 'Bleu' },
-    { marque: 'Ford', couleur: 'Vert' }
-]);
+
 
 function MyCars({title}) {
+
+    const [cars, setCar] = useState([
+        { marque: 'Renault', couleur: 'Rouge' },
+        { marque: 'Honda', couleur: 'Bleu' },
+        { marque: 'Ford', couleur: 'Vert' }
+    ]);
+    
     return (
         <Wrapper>
             <h2>{title}</h2>
