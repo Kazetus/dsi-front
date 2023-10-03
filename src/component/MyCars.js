78d92058c9749dbeab56c3d9a1524d605128b66d
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-10-03 10:22:57 +0200

    add array and map

--- src/component/MyCars.js
+++ src/component/MyCars.js
@@ -2,13 +2,25 @@ import React from 'react';
 import Wrapper from './Wrapper';
 import Car from './Car';
 
+const [cars, setCar] = useState([
+    { marque: 'Renault', couleur: 'Rouge' },
+    { marque: 'Honda', couleur: 'Bleu' },
+    { marque: 'Ford', couleur: 'Vert' }
+]);
+
 function MyCars({title}) {
     return (
         <Wrapper>
             <h2>{title}</h2>
-            <Car marque="Ford" couleur="vert" />
+            {cars.map((car) => (
+                <>
+                <Car marque={car.marque} couleur={car.couleur} />
+                <hr/>
+                </>
+            ))}
+            {/* <Car marque="Ford" couleur="vert" />
             <Car marque="Renault" couleur="rouge" />
-            <Car marque="Citroen" couleur="bleu" />
+            <Car marque="Citroen" couleur="bleu" /> */}
         </Wrapper>
     );
 }
